bcf942df2aca6d1532e690af49bffadfc906b92e
redapy <baha.redaaaa@gmail.com>
2022-06-03 18:36:03 +0100

    render the components conditionally

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,13 @@ import './App.css';
 import { searchArtworks } from '../utils/api';
 import { SearchForm } from './SearchForm';
 import { Footer } from './Footer';
+import { useState } from 'react';
+import ArtWorkList from './ArtWorkList';
+import ImageDetailsPage from './ImageDetailsPage';
 
 export function App() {
+	const [data, setData] = useState(null);
+	const [selectedArt, setSelectedArt] = useState(null);
 	function onSearchSubmit(query) {
 		// Search for the users's query.
 		// TODO: render the results, instead of logging them to the console.
@@ -13,14 +18,23 @@ export function App() {
 		// our UI, we need to make real requests!
 		// @see: ./src/uitls/api.js
 		searchArtworks(query).then((json) => {
-			console.log(json);
+			setData(json.data);
 		});
 	}
 
 	return (
 		<div className="App">
 			<h1>TCL Career Lab Art Finder</h1>
-			<SearchForm onSearchSubmit={onSearchSubmit} />
+			{!selectedArt && <SearchForm onSearchSubmit={onSearchSubmit} />}
+			{data && !selectedArt && (
+				<ArtWorkList data={data} setSelectedArt={setSelectedArt} />
+			)}
+			{selectedArt && (
+				<ImageDetailsPage
+					imageId={selectedArt.image_id}
+					setSelectedArt={setSelectedArt}
+				/>
+			)}
 			<Footer />
 		</div>
 	);
